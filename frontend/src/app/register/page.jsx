60586e8f086f1ac5365b9e3656c0e333d4755a7e
Emadedin Elmontaser <emadelmontaser1@gmail.com>
2025-07-26 17:13:17 -0400

    Create page.jsx

---
+++ frontend/src/app/register/page.jsx
@@ -0,0 +1,6 @@
+import LoginPage from '../../components/LoginPage';
+
+export default function Register() {
+  // Pass mode="register" so it shows the registration form by default
+  return <LoginPage mode="register" />;
+}
