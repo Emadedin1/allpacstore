8c837502a879c80b861c10630dca9ab6ac4c7179
Emadedin Elmontaser <emadelmontaser1@gmail.com>
2025-08-12 00:15:51 -0400

    Update ProductSections.jsx

--- src/components/ProductSections.jsx
+++ src/components/ProductSections.jsx
@@ -5,18 +5,20 @@ import { cups } from "../data/cups";
 
 export default function ProductSections() {
   return (
-    <div
-      className="
-        grid
-        grid-cols-2          /* 2 cols on mobile */
-        lg:grid-cols-4       /* 4 cols on desktop */
-        gap-3 lg:gap-6
-        lg:auto-rows-fr      /* match homepage: only on lg */
-      "
-    >
-      {cups.map((cup) => (
-        <CupCard key={cup.slug} cup={cup} />
-      ))}
+    <div className="max-w-7xl mx-auto px-4">
+      <div
+        className="
+          grid
+          grid-cols-2
+          lg:grid-cols-4
+          gap-3 lg:gap-6
+          lg:auto-rows-fr
+        "
+      >
+        {cups.map((cup) => (
+          <CupCard key={cup.slug} cup={cup} />
+        ))}
+      </div>
     </div>
   );
 }
