a43e0f0650e785e0696d6d317ee47da36ad709a1
Emadedin Elmontaser <emadelmontaser1@gmail.com>
2025-08-12 15:05:20 -0400

    Update SiteBanner.jsx

--- src/components/SiteBanner.jsx
+++ src/components/SiteBanner.jsx
@@ -65,7 +65,7 @@ export default function SiteBanner() {
         left: 0,
         top: 0,
         zIndex: 9999,
-        background: '#F5F5F5',
+        background: '#E6F2FA',
         color: '#ffffff',
         padding: '10px 0',
         textAlign: 'center',
