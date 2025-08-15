9371c3e1566a9e3ebe299a117230a74e97f10ec9
Emadedin Elmontaser <emadelmontaser1@gmail.com>
2025-08-14 20:23:06 -0400

    Update page.jsx

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -53,7 +53,7 @@ export default function ProductsCategoriesPage() {
                 "
               />
               {/* Softer gradient (reduced opacity & darkness) */}
-              <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-black/25 via-black/5 to-transparent" />
+              <div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/4 bg-gradient-to-t from-black/50 to-transparent" />
               {/* Title with lighter shadow/glow */}
               <div className="absolute inset-x-0 bottom-0 p-2.5 sm:p-3 flex justify-center">
                 <span
