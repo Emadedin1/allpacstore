d8db90c8523772436237661ac9b39067b6e6868a
Emadedin Elmontaser <emadelmontaser1@gmail.com>
2025-10-29 16:39:30 -0400

    Refactor ProductSections component layout

--- src/components/ProductSections.jsx
+++ src/components/ProductSections.jsx
@@ -6,15 +6,7 @@ import { cups } from "../data/cups";
 export default function ProductSections() {
   return (
     <div className="max-w-7xl mx-auto px-4">
-      <div
-        className="
-          grid
-          grid-cols-2
-          lg:grid-cols-4
-          gap-3 lg:gap-6
-          lg:auto-rows-fr
-        "
-      >
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-6 lg:auto-rows-fr w-full">
         {cups.map((cup) => (
           <CupCard key={cup.slug} cup={cup} />
         ))}
