6d57e0e2755f11898031526b3099948cd625c20f
Emadedin Elmontaser <emadelmontaser1@gmail.com>
2025-10-29 17:54:28 -0400

    Update ProductSections.jsx

--- src/components/ProductSections.jsx
+++ src/components/ProductSections.jsx
@@ -5,18 +5,22 @@ import { cups } from "../data/cups";
 
 export default function ProductSections() {
   return (
-    <div className="max-w-7xl mx-auto px-4">
+    <div className="max-w-6xl mx-auto px-2 sm:px-4">
       <div
         className="
           grid
           grid-cols-2
-          lg:grid-cols-3
-          gap-3 lg:gap-6
-          lg:auto-rows-fr
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          gap-2 sm:gap-3 lg:gap-5
+          justify-items-center
         "
       >
         {cups.map((cup) => (
-          <CupCard key={cup.slug} cup={cup} />
+          <div key={cup.slug} className="w-full max-w-[170px] sm:max-w-[200px]">
+            <CupCard cup={cup} />
+          </div>
         ))}
       </div>
     </div>
