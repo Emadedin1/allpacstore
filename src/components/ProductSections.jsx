0a6621cc6537406127dee589cff33d5b8d91da1d
Emadedin Elmontaser <emadelmontaser1@gmail.com>
2025-10-29 16:44:54 -0400

    Update ProductSections.jsx

--- src/components/ProductSections.jsx
+++ src/components/ProductSections.jsx
@@ -10,7 +10,7 @@ export default function ProductSections() {
         className="
           grid
           grid-cols-2
-          lg:grid-cols-4
+          lg:grid-cols-3
           gap-3 lg:gap-6
           lg:auto-rows-fr
         "
