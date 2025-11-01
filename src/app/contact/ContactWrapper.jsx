744e61b1408e55f68d72352bf31c1e50997e457a
Emadedin Elmontaser <emadelmontaser1@gmail.com>
2025-11-01 16:06:26 -0400

    Delete src/app/contact/ContactWrapper.jsx

--- src/app/contact/ContactWrapper.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-
-// ✅ Dynamically import ContactClient only in the browser
-const ContactClient = dynamic(() => import("./ContactClient"), { ssr: false });
-
-export default function ContactWrapper() {
-  return <ContactClient />;
-}
