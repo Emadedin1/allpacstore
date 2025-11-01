b1ba132bd5b96a0bd4e6429311b17b2cf00d9e39
Emadedin Elmontaser <emadelmontaser1@gmail.com>
2025-11-01 15:18:46 -0400

    Create AboutClient.jsx

---
+++ src/app/about/AboutClient.jsx
@@ -0,0 +1,11 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function AboutClient() {
+  return (
+    <section className="pt-4 pb-10 px-4">
+      {/* your full existing content */}
+    </section>
+  );
+}
